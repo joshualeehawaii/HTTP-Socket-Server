3b37426f8ba49eafadfb74ec3f02ffc7d37089cb
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-06 22:00:43 -1000

    added client updates

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 //1. Create a client to establish TCP socket connections to HTTP servers
 //2. The node command requires a single argument, the host and uri to request a resource from
-//3. example: www.devleague.com/apply
+//example: www.devleague.com/apply
 //4. Transmit 'standard' HTTP Headers to the server
 //5. Wait for a response from the server
 //6. When the server responds, display the response message body to the terminal
@@ -8,7 +8,18 @@
 /*jshint esversion: 6*/
 
 const net = require('net');
+console.log(process.argv); //array
+//'GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n'
 
-const client = net.createConnection({port: 8080, host: '0.0.0.0'}, () => {
+const port = 80;
+const destination = process.argv[2];
+
+const client = net.createConnection({port: port, host: destination}, () => {
   console.log('connected');
+});
+
+client.write(`GET / HTTP/1.1\r\nHost: ${destination}\r\nConnection: close\r\n\r\n`);
+
+client.on('data', (data) => {
+  process.stdout.write(data);
 });
