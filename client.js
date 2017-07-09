1f2f66f780daa0b8e6f3f815eb91bf6e1ad40a6f
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-08 15:53:42 -1000

    client is still a mess

--- client.js
+++ client.js
@@ -8,18 +8,55 @@
 /*jshint esversion: 6*/
 
 const net = require('net');
-console.log(process.argv); //array
+
 //'GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n'
 
 const port = 80;
 const destination = process.argv[2];
+var host = destination;
+console.log('this is the argv',process.argv); //array
 
-const client = net.createConnection({port: port, host: destination}, () => {
+
+const client = net.createConnection({port: port, host: destination}, () => { //build path to URI
   console.log('connected');
 });
 
-client.write(`GET / HTTP/1.1\r\nHost: ${destination}\r\nConnection: close\r\n\r\n`);
+client.write(`GET /${host} HTTP/1.1\r\nHost: ${destination}\r\nConnection: close\r\n\r\n`);
+
 
 client.on('data', (data) => {
   process.stdout.write(data);
 });
+
+
+//fing the first slash, and anying after is a URI
+
+if (destination.indexOf('/') > -1) {
+  host = destination.slice(0, destination.indexOf('/'));
+} else {
+  host = destination;
+}
+
+// const net = require('net')
+
+// const target = process.argv[2]
+
+// let host = target.indexOf('/') > -1 ? target.slice(0, target.indexOf('/')) : target
+// let uri = target.indexOf('/') > -1 ? target.slice(target.indexOf('/')) : '/'
+
+// let request = net.createConnection(80, host)
+
+// request.setEncoding('utf8')
+
+// request.on('connect', function(){
+//   process.stdout.write('connectd')
+//   request.write(`GET ${uri} HTTP/1.1\nHost: ${host}\nConnection: Close\n\n`);
+// })
+
+// request.on('data', function(data) {
+//   process.stdout.write(data.toString())
+// })
+
+// request.on('end', function(){
+//   process.stdout.write('\n\nconnection terminated')
+// })
